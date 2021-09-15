133ad680a3100aedd40c73c975940394cc8b7d3f
Wonjaegang <robinsam@naver.com>
2021-09-15 13:43:21 +0900

    Initial commit

---
+++ wonjaegang/2_Velocity&Acceleration.py
@@ -0,0 +1,10 @@
+# 2. Velocity and Acceleration - 기출
+#     x 축 위의 원점으로부터 20m만큼 떨어진 곳에 정점이 있다. 정점은 가속도 5m/s^2, 최대속도 20m/s로 오른쪽으로 이동하며 원점으로부터
+#     오른쪽으로 400m 떨어진 곳에 도달한다. 도달 전에 정점은 -5m/s^2으로 가속하며 속도를 줄이다가 도달지점에서 속도는 0이 된다. 정점의
+#     거리, 속도, 가속도 그래프를 그리시오.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    pass
