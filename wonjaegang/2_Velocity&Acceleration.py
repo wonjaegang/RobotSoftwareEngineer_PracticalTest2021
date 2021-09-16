040ff5a32c78dc5c2802ccf0e29821b1806b2ee0
Wonjaegang <robinsam@naver.com>
2021-09-16 22:29:48 +0900

    풀이 끝
    그래프를 더 편하게 그리는 법 찾기

--- wonjaegang/2_Velocity&Acceleration.py
+++ wonjaegang/2_Velocity&Acceleration.py
@@ -6,5 +6,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['figure.figsize'] = (5, 10)
+
 if __name__ == "__main__":
-    pass
+    # 등가속도 운동의 그래프
+    def plotGraph(t1, t2, a0, v0, s0):
+        t = np.linspace(t1, t2)
+        a = a0 + (t - t1) * 0
+        v = v0 + (t - t1) * a
+        s = s0 + (t - t1) * v0 + ((t - t1) ** 2) * a * 0.5
+        p1.plot(t, s, 'b')
+        p2.plot(t, v, 'r')
+        p3.plot(t, a, 'g')
+
+    # 플롯 스타일
+    fig = plt.figure()
+    p1 = fig.add_subplot(3, 1, 1)
+    p2 = fig.add_subplot(3, 1, 2)
+    p3 = fig.add_subplot(3, 1, 3)
+    p1.set_xlabel("Distance")
+    p1.set_ylabel("Time")
+    p2.set_xlabel("Velocity")
+    p2.set_ylabel("Time")
+    p3.set_xlabel("Acceleration")
+    p3.set_ylabel("Time")
+
+    # 가속 구간
+    plotGraph(0, 4, 5, 0, 20)
+
+    # 등속 구간
+    plotGraph(4, 19, 0, 20, 60)
+
+    # 감속 구간'
+    plotGraph(19, 23, -5, 20, 360)
+
+    plt.show()
