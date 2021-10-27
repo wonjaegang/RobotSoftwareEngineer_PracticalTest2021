73adf1ea11e46755426f2cf502d69c91c292ad44
Wonjaegang <robinsam@naver.com>
2021-10-27 11:29:14 +0900

    시간에 따른 각도함수(Cubic spline)를 계산하는 함수를 추가하였다. 이를 이용해, 시간에 따른 매니풀레이터의 자세를 출력하는 코드도 추가하였다.

--- wonjaegang/Part1-1.py
+++ wonjaegang/Part1-1.py
@@ -1,4 +1,4 @@
-# 1) 액추에이터의 각도에 따른 매니퓰레이터의 자세출력
+# 1) 액추에이터의 각도에 따른 매니퓰레이터의 자세출력 - 2차원 매니퓰레이터
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 # 매니풀레이터 기본 사양
 # 2차원 다관절 매니퓰레이터, 링크 초기위치 그림참고
 l1, l2, l3 = 2, 1, 1
-# 3차원 다관절 매니퓰레이터
 
 
 # DH 파라미터
@@ -46,9 +45,19 @@ def T23(motor3):
 
 def jointLocation(m1, m2, m3):
     P0 = np.array([0, 0, 0, 1])
-    x = [P0[0], (T01(m1) @ P0)[0], (T01(m1) @ T12(m2) @ P0)[0], (T01(m1) @ T12(m2) @ T23(m3) @ P0)[0]]
-    y = [P0[1], (T01(m1) @ P0)[1], (T01(m1) @ T12(m2) @ P0)[1], (T01(m1) @ T12(m2) @ T23(m3) @ P0)[1]]
-    return x, y
+    X = [P0[0], (T01(m1) @ P0)[0], (T01(m1) @ T12(m2) @ P0)[0], (T01(m1) @ T12(m2) @ T23(m3) @ P0)[0]]
+    Y = [P0[1], (T01(m1) @ P0)[1], (T01(m1) @ T12(m2) @ P0)[1], (T01(m1) @ T12(m2) @ T23(m3) @ P0)[1]]
+    return X, Y
+
+
+# 시간에 대한 각도함수 계산(Cubic spline)
+def motorAngle(w0, wf, tf, t):
+    a0 = w0
+    a1 = 0
+    a2 = 3 / (tf ** 2) * (wf - w0)
+    a3 = -2 / (tf ** 3) * (wf - w0)
+    w = a0 + a1 * t + a2 * (t ** 2) + a3 * (t ** 3)
+    return w
 
 
 if __name__ == "__main__":
@@ -56,6 +65,7 @@ if __name__ == "__main__":
     np.set_printoptions(formatter={'float_kind': lambda x: "{0:0.3f}".format(x)})
 
     # 매니퓰레이터 자세 출력
+    plt.figure(1)
     plt.axes().set_aspect('equal')
 
     plot1 = jointLocation(0, 0, 0)
@@ -65,4 +75,19 @@ if __name__ == "__main__":
     plt.plot(plot2[0], plot2[1])
 
     plt.legend(["Before", "After"])
+
+    # 시간에 따른 각도 출력(0 ~ pi/3)
+    plt.figure(2)
+    time = np.linspace(0, 10)
+    plt.plot(time, motorAngle(0, np.pi / 3, 10, time))
+
+    # 시간에 따른 매니퓰레이터 자세 출력
+    plt.figure(3)
+    time = np.linspace(0, 10, 20)
+    for i in time:
+        plot3 = jointLocation(motorAngle(0, np.pi / 3, 10, i),
+                              motorAngle(0, np.pi / 3, 10, i),
+                              motorAngle(0, np.pi / 3, 10, i))
+        plt.plot(plot3[0], plot3[1])
+
     plt.show()
