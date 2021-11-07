ecc84108effbd1bf62fa4208e3e96f67c42e6063
Wonjaegang <robinsam@naver.com>
2021-11-08 00:21:51 +0900

    part4-1 1차 연습 끝!

--- wonjaegang/EmptyFile_practice.py
+++ wonjaegang/EmptyFile_practice.py
@@ -1,159 +1,78 @@
-# PSO 3D 연습
+# 가속운동하는 물체의 특성 출력
 
-import numpy as np
 import matplotlib.pyplot as plt
-import random
 
-# 매니퓰레이터 기본 사양
-l1, l2, l3, l4, l5, l6 = 4, 1, 2, 1, 1, 1
-
-
-# DH parameter
-def DH_parameter(theta, d, a, alpha):
-    T_theta = np.array([[np.cos(theta), -np.sin(theta), 0, 0],
-                        [np.sin(theta), np.cos(theta), 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 0, 0, 1]])
-    T_d = np.array([[1, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, d],
-                    [0, 0, 0, 1]])
-    T_a = np.array([[1, 0, 0, a],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1]])
-    T_alpha = np.array([[1, 0, 0, 0],
-                        [0, np.cos(alpha), -np.sin(alpha), 0],
-                        [0, np.sin(alpha), np.cos(alpha), 0],
-                        [0, 0, 0, 1]])
-    return T_theta @ T_d @ T_a @ T_alpha
+# 물체 특성 데이터시트
+s0 = [0, 0, 0, 20, 30]
+v0 = [0, 10, 20, 0, 0]
+a0 = [5, 1, 0, 5, 2]
+v_max = [10, 20, 20, 15, 10]
+s1 = [100, 100, 100, 100, 100]
+v1 = [0, 10, 20, 0, 0]
+a1 = [-5, -1, 0, -10, -2]
 
 
-# Homogeneous transformation
-def T01(motor1):
-    return DH_parameter(motor1, l1, 0, np.pi / 2)
-
-
-def T12(motor2):
-    return DH_parameter(motor2, -l2, l3, 0)
-
-
-def T23(motor3):
-    return DH_parameter(motor3, 0, l4, -np.pi / 2)
-
-
-def T34(motor4):
-    return DH_parameter(motor4 + np.pi / 2, 0, 0, np.pi / 2)
-
-
-def T45(motor5):
-    return DH_parameter(motor5, l5 + l6, 0, 0)
-
-
-# Location of each joint
-def jointLocation(m1, m2, m3, m4, m5):
-    P0 = np.array([[1, 0, 0, 0],
-                   [0, 1, 0, 0],
-                   [0, 0, 1, 0],
-                   [0, 0, 0, 1]])
-    P1 = T01(m1) @ P0
-    P2 = T01(m1) @ T12(m2) @ P0
-    P3 = T01(m1) @ T12(m2) @ T23(m3) @ P0
-    P4 = T01(m1) @ T12(m2) @ T23(m3) @ T34(m4) @ P0
-    P5 = T01(m1) @ T12(m2) @ T23(m3) @ T34(m4) @ T45(m5) @ P0
-    X = [p[0, 3] for p in [P0, P1, P2, P3, P4, P5]]
-    Y = [p[1, 3] for p in [P0, P1, P2, P3, P4, P5]]
-    Z = [p[2, 3] for p in [P0, P1, P2, P3, P4, P5]]
-    return X, Y, Z
-
-
-# Inverse kinematics using PSO
-def IK_PSO(T_target):
-
-    plt.ion()
-    figure = plt.subplots(figsize=(8, 6))[0]
-    ax = plt.axes(projection='3d')
-
-    def loss(d1, d2, d3, d4, d5):
-        T_now = T01(d1) @ T12(d2) @ T23(d3) @ T34(d4) @ T45(d5)
-        k = 0.5
-        unitP = np.array([[0],
-                          [0],
-                          [0],
-                          [1]])
-        unitR = np.array([[1, 0, 0],
-                          [0, 1, 0],
-                          [0, 0, 1],
-                          [0, 0, 0]])
-        Ep = (T_now @ unitP - T_target @ unitP)[:3].__abs__().max() / (T_target @ unitP)[:3].__abs__().max()
-        Er = (T_now @ unitR - T_target @ unitR)[:3].__abs__().max() / (T_target @ unitR)[:3].__abs__().max()
-        z = k * Ep + (1 - k) * Er
-        return z
-
-    targetError = 0.00000001
-    population = 50
-    particleS = [[random.uniform(-np.pi / 2, np.pi / 2) for _ in range(5)] for _ in range(population)]
-    particleV = [[random.uniform(-np.pi / 2, np.pi / 2) for _ in range(5)] for _ in range(population)]
-    particleBest = [[random.uniform(-np.pi / 2, np.pi / 2) for _ in range(5)] for _ in range(population)]
-    particleBestValue = [loss(*best) for best in particleBest]
-    globalBest = [random.uniform(-np.pi / 2, np.pi / 2) for _ in range(5)]
-    globalBestValue = loss(*globalBest)
-
-    error = float('inf')
-    count = 0
-    while error > targetError:
-        for i in range(population):
-            w = 0.4
-            c1 = 0.8
-            r1 = random.uniform(0, 1)
-            c2 = 0.8
-            r2 = random.uniform(0, 1)
-            particleV[i] = [w * vi + c1 * r1 * (pb - si) + c2 * r2 * (gb - si)
-                            for si, vi, pb, gb in zip(particleS[i], particleV[i], particleBest[i], globalBest)]
-            particleS[i] = [si + vi for si, vi in zip(particleS[i], particleV[i])]
-
-            loss_now = loss(*particleS[i])
-            if loss_now < particleBestValue[i]:
-                particleBest[i] = particleS[i]
-                particleBestValue[i] = loss_now
-                if loss_now < globalBestValue:
-                    globalBest = particleS[i]
-                    globalBestValue = loss_now
-
-            ax.plot(*jointLocation(*particleS[i]))
-
-        error = globalBestValue
-        count += 1
-        print("No %d best loss: %.8f" % (count, error))
-
-
-        figure.canvas.draw()
-        figure.canvas.flush_events()
-        plt.cla()
-
-    return globalBest
+def round1(num):
+    return round(num, 1)
 
 
 if __name__ == "__main__":
-    # plt.figure(1)
-    # ax = plt.axes(projection='3d')
-    #
-    # ax.plot(*jointLocation(0, 0, 0, 0, 0))
-    #
-    T1 = np.array([[0, 0, 1, 3],
-                   [0, -1, 0, 1],
-                   [1, 0, 0, 6],
-                   [0, 0, 0, 1]])
-    T2 = np.array([[1, 0, 0, 2],
-                   [0, 0, -1, -1],
-                   [0, 1, 0, 5],
-                   [0, 0, 0, 1]])
-    IK_PSO(T1)
-    # ax.plot(*jointLocation(*IK_PSO(T2)))
-    #
-    # ax.set_xlabel('x')
-    # ax.set_ylabel('y')
-    # ax.set_zlabel('z')
-    # ax.set_title("Manipulator pose")
-    #
-    # plt.show()
+    # 각 물체마다
+    for i in range(5):
+        # 물체 특성 초기화
+        dt = 0.1
+        s, v, a = [s0[i]], [v0[i]], [a0[i]]
+        state = "acceleration"
+
+        # 메인 루프
+        while state != "end":
+            # 속도 업데이트
+            v.append(v[-1] + a[-1] * dt)
+
+            # 위치 업데이트
+            s.append(s[-1] + v[-1] * dt)
+
+            # 가속도 및 상태 업데이트
+            # 가속 상태
+            if state == "acceleration":
+                # 가속 -> 감속
+                if round1(2 * a1[i] * (s1[i] - s[-1])) >= round1((v1[i] - v[-1]) * (v1[i] + v[-1])):
+                    state = "deceleration"
+                    a.append(a1[i])
+                # 가속 -> 최고속도
+                elif round1(v[-1]) == round1(v_max[i]):
+                    state = "max velocity"
+                    a.append(0)
+                # 가속 -> 가속
+                else:
+                    a.append(a0[i])
+
+            # 최고속도
+            if state == "max velocity":
+                # 최고속도 -> 감속
+                if round1(2 * a1[i] * (s1[i] - s[-1])) >= round1((v1[i] - v[-1]) * (v1[i] + v[-1])):
+                    state = "deceleration"
+                    a.append(a1[i])
+                # 최고속도 -> 최고속도
+                else:
+                    a.append(0)
+            # 감속 상태
+            if state == "deceleration":
+                # 감속 -> 도착
+                if round1(s[-1]) >= round1(s1[i]):
+                    state = "end"
+                    a.append(a1[i])
+                # 감속 -> 감속
+                else:
+                    a.append(a1[i])
+
+        plt.figure(1)
+        plt.plot([dt * i for i in range(len(s))], s)
+
+        plt.figure(2)
+        plt.plot([dt * i for i in range(len(v))], v)
+
+        plt.figure(3)
+        plt.plot([dt * i for i in range(len(a))], a)
+
+    plt.show()
